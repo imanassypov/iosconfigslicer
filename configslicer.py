9d973e9fcf838c951d59e7d486f0008fafd27639
Igor Manassypov <imanassy@cisco.com>
2022-02-24 11:22:06 -0500

    added license

--- configslicer.py
+++ configslicer.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2021 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+__author__ = "Igor Manassypov, System Architect"
+__email__ = "imanassy@cisco.com"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2021 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
 from ciscoconfparse import CiscoConfParse
 from netmiko import ConnectHandler
 import time
